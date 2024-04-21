34e2f9f5d63b16e814d287bc24e9987d1bed5d6d
mumarsaleem <umarsaleem979@gmail.com>
2024-04-21 08:44:26 +0200

    update permissions commands

--- permissions_commands.sql
+++ permissions_commands.sql
@@ -1,3 +1,6 @@
 -- example GRANT SELECT permissions.
 
 grant select on table sales to fred;
+
+-- Use hierarchical approach by using specific schemas and roles.
+GRANT USAGE FOR SCHEMAS IN DATABASE Sales_db TO ROLE Sales;
